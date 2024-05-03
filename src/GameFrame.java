e6746c2c4a559e484692bb53735ff3ade914087b
Lapo <patrick.pernisi@supsis.ch>
2024-05-03 11:42:18 +0200

    Add Time class and implemented run() method in GameFrame class

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -6,12 +6,17 @@ public class GameFrame extends JFrame implements Runnable {
     private static GameFrame instance = null;
     public static final int WIDTH = 800;
     public static final int HEIGHT = 800;
+    public static final int FRAMES_PER_SECOND = 30;
+    private volatile boolean shutdown = false;
 
     private GameFrame() {
-        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        setTitle("Super Tic Tac Toe");
+        setSize(new Dimension(WIDTH, HEIGHT));
         setResizable(false);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setVisible(true);
     }
 
     public static GameFrame getInstance() {
@@ -21,8 +26,26 @@ public class GameFrame extends JFrame implements Runnable {
         return instance;
     }
 
+    private void update(double dt) {
+
+    }
+
     @Override
     public void run() {
+        double lastFrameTime = 0.0;
+        while (!shutdown) {
+            double time = Time.getTime();
+            double deltaTime = time - lastFrameTime;
+            lastFrameTime = time;
 
+            update(deltaTime);
+
+            try {
+                Thread.sleep(FRAMES_PER_SECOND);
+            } catch (Exception e) {
+
+            }
+
+        }
     }
 }
